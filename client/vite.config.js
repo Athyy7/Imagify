6b4d9555139ad426b866b0738fddefe4dabdb478
Athyy7 <atharav.shivhare009@gmail.com>
2025-06-30 01:21:41 +0530

    changed vite file

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,6 +5,10 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
+    host: '0.0.0.0',          
+    port: 5173,            
+    strictPort: true,         
+    open: false, 
     historyApiFallback: true  // Fixes 404 on reload for client-side routes
   }
 })
